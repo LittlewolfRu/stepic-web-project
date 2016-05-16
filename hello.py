f474ea3ad6f0a1462e74a9f391e3b94b2ddb8eb6
vasmitr <vasmitr95@gmail.com>
2016-05-16 17:16:44 +0000

    modified:   etc/nginx.conf 	modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 
 def hello_app(env, start_response):
-    body = [
-        '%s=%s' % (key, value) for key, value in env.items()
-    ]
+    body = [p for p in env['QUERY_STRING'].split('&')]
     body = '\n'.join(body)
     status = '200 OK'
     response_headers = [('Content-Type', 'text/plain'),
